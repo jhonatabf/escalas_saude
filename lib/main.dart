cafacd9851612bfe6c6246fb6d63a59c4443ebbf
jhonata.reboucas <jhonatareboucas@gmail.com>
2019-09-06 13:33:51 -0300

    primeiro commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,201 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+String tituloApp = "Minhas escalas";
+String sloganApp = "Sua solução para verificar a condição do paciente";
+final titulosEscalas = ['Braden', 'MEWS'];
+final paginas = ['braden', 'mews'];
+final coresincone = [Colors.orange, Colors.cyan];
+final subtitulosEscalas = [
+  'Risco para Úlcera por Pressão',
+  'National Early Warning Score'
+];
+int _selectedIndex = 0;
+const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+const List<Widget> _widgetOptions = <Widget>[
+  Text(
+    'Index 0: Escalas',
+    style: optionStyle,
+  ),
+  Text(
+    'Index 1: Hospitais',
+    style: optionStyle,
+  ),
+  Text(
+    'Index 2: Pacientes',
+    style: optionStyle,
+  ),
+];
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Escalas de Saúde',
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Escalas de Saúde'),
-    );
-  }
+void main() {
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  runApp(MaterialApp(
+    title: tituloApp,
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class _HomeState extends State<Home> {
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text(widget.title, style: TextStyle(color: Colors.grey[700]),),
-      iconTheme: IconThemeData(
-        color: Colors.grey[700],
+        elevation: 0.5,
+        title: Text(
+          tituloApp,
+          style: TextStyle(color: Colors.grey[700]),
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.grey[700],
+        ),
       ),
-      ),
-      body: Center(),drawer: Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          UserAccountsDrawerHeader(
-            accountName: Text("Jhonata Rebouças"),
-            accountEmail: Text("jhonatareboucas@gmail.com"),
-            decoration: BoxDecoration(
-              color: Colors.green,
+      body: base(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text(tituloApp),
+              accountEmail: Text(sloganApp),
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text(
+                  "Ms",
+                  style: TextStyle(fontSize: 40.0, color: Colors.green),
+                ),
+              ),
             ),
-            currentAccountPicture: CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Text(
-                "Jh",
-                style: TextStyle(fontSize: 40.0, color: Colors.green),
+            ListTile(
+              leading: Icon(
+                Icons.info_outline,
+                color: Colors.green,
+                size: 40.0,
+              ),
+              title: Text("Sobre o Aplicativo",
+                  style: TextStyle(
+                      color: Colors.grey[600], fontWeight: FontWeight.bold)),
+              subtitle: Text(
+                "O que podemos ajudar?",
+                style: TextStyle(color: Colors.grey[500]),
+              ),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(
+                Icons.developer_mode,
+                color: Colors.green,
+                size: 40.0,
+              ),
+              title: Text("Sobre o Desenvolvedor",
+                  style: TextStyle(
+                      color: Colors.grey[600], fontWeight: FontWeight.bold)),
+              subtitle: Text(
+                "O que podemos fazer por você?",
+                style: TextStyle(color: Colors.grey[500]),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Escalas'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Hospitais'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.perm_contact_calendar),
+            title: Text('Pacientes'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget base() {
+    return Column(
+      children: <Widget>[
+        Container(
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.fromLTRB(14, 20, 12, 5),
+          child: Text(
+            "ESCALAS DISPONÍVEIS",
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+          decoration: const BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                width: 1.0,
+                color: Color(0xFFFFDDDDDD),
               ),
             ),
           ),
-          Divider(),
-          ListTile(
-            title: Text('Sobre o desenvolvedor'),
+        ),
+        Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: titulosEscalas.length,
+            itemBuilder: (context, index) {
+              int item = index;
+              return Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      width: 1.0,
+                      color: Color(0xFFFFDDDDDD),
+                    ),
+                  ),
+                ),
+                child: ListTile(
+                  onTap: () {},
+                  leading: Icon(
+                    Icons.content_copy,
+                    color: coresincone[index],
+                    size: 30.0,
+                  ),
+                  title: Text(titulosEscalas[index].toString(),
+                      style: TextStyle(
+                          color: Colors.grey[600],
+                          fontWeight: FontWeight.bold)),
+                  subtitle: Text(
+                    subtitulosEscalas[index].toString(),
+                    style: TextStyle(color: Colors.grey[500]),
+                  ),
+                ),
+              );
+            },
           ),
-          ListTile(
-            subtitle: Text.rich(
-              TextSpan(text: "oi", children: <TextSpan>[
-                TextSpan(text: "LinkedIn", style: TextStyle(color: Colors.green)),
-              ],),
-            ),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Sobre o aplicativos'),
-          ),
-          ListTile(
-            subtitle: Text("sobreApp"),
-          ),
-          Divider(),
-        ],
-      ),
-    ),
+        ),
+      ],
+
     );
   }
 }
+
